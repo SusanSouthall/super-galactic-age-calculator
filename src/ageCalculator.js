a1acc6fadd832575116613f5d50788b6c6278cfa
Susan Southall <susan@Susans-MBP.hsd1.or.comcast.net>
2018-04-20 17:59:50 -0700

    Clean extra comments out

--- src/ageCalculator.js
+++ src/ageCalculator.js
@@ -1,20 +1,15 @@
 class Age {
   constructor(birthdate) {
     this.birthdate = new Date(birthdate);
-  
-  }
 
+  }
+  // current age in seconds
   birthdateToSeconds() {
     let todaysDate = new Date();
     let seconds = Math.round(todaysDate - this.birthdate);
     return seconds;
   }
-  // current age in seconds
   currentAge() {
-    // const minutes = 1000 * 60;
-    // const hours = minutes * 60;
-    // const days = hours * 24;
-    // const years = days * 365; // milliseconds in a year
     let todaysDate = new Date();
     let age = todaysDate.getFullYear() - this.birthdate.getFullYear();
     return this.age = age;
