1d249103f3401ecf3e3359bbe92733691e287ea7
Susan Southall <susan@Susans-MBP.hsd1.or.comcast.net>
2018-04-22 12:54:14 -0700

    Add 8th test to ageCalculator-spec.js

--- spec/ageCalculator-spec.js
+++ spec/ageCalculator-spec.js
@@ -36,4 +36,8 @@ describe("Age", function(){
     it("should return a person's life expectancy for Mercury", function() {
       expect(birthdate.averageLifeExpectancy("mercury", 80)).toEqual(150);
   });
+
+    it("should return a person's life expectancy for Venus", function() {
+    expect(birthdate.averageLifeExpectancy("venus", 80)).toEqual(150);
+  });
 });
